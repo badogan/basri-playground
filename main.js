88bb43a4b069644df37b792ebc47496a5cdfecee
Basri Dogan <basri.dogan.71@gmail.com>
2023-10-04 10:19:37 +0100

    changed to Portugal. closes #18

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const bedrock = new AWS.BedrockRuntime({
 // const modelId1 = 'amazon.titan-tg1-large'; // Replace with the desired model ID
 const modelId2 = "ai21.j2-ultra-v1"; // Replace with the desired model ID
 
-const promptReceived = "What is the capital city of Finland?"
+const countrySize = "largest"
+const countryName = "Portugal"
+
+const promptReceived = `What is the ${countrySize} city of ${countryName}?`
 
 const bodyForModelId2 = {
   prompt: promptReceived,
