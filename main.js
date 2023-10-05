348d5399b76536b29d09604e0840ebc8e2025be9
Basri Dogan <basri.dogan.71@gmail.com>
2023-10-05 10:39:28 +0100

    changes to smallest

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const bedrock = new AWS.BedrockRuntime({
 // const modelId1 = 'amazon.titan-tg1-large'; // Replace with the desired model ID
 const modelId2 = "ai21.j2-ultra-v1"; // Replace with the desired model ID
 
-const countrySize = "shiniest"
+const countrySize = "smallest"
 const countryName = "Greece"
 
 const promptReceived = `What is the ${countrySize} city of ${countryName}?`
