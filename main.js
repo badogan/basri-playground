2a93c2e379d0124de7b1c87ce733155d323e8988
Basri Dogan <basri.dogan.71@gmail.com>
2023-10-05 09:51:08 +0100

    Closes #32. Closes #33

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const bedrock = new AWS.BedrockRuntime({
 // const modelId1 = 'amazon.titan-tg1-large'; // Replace with the desired model ID
 const modelId2 = "ai21.j2-ultra-v1"; // Replace with the desired model ID
 
-const countrySize = "smallest"
-const countryName = "Switzerland"
+const countrySize = "smoothest"
+const countryName = "Portugal"
 
 const promptReceived = `What is the ${countrySize} city of ${countryName}?`
 
